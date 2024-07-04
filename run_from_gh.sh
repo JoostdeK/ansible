fdd06107ae0ab7a2305f500a19ec1769d5fe5508
JoostdeK <110180328+JoostdeK@users.noreply.github.com>
2024-07-04 22:10:15 +0200

    Update run_from_gh.sh

--- run_from_gh.sh
+++ run_from_gh.sh
@@ -3,7 +3,7 @@ apt update && apt install sudo
 
 sudo apt-install software-properties-common
 ##install basics and ansible
-sudo apt-add-repository -y ppa:ansible/ansible && \
+sudo apt add-repository -y ppa:ansible/ansible && \
     apt-get update && \
     apt-get install -y curl git ansible
 
